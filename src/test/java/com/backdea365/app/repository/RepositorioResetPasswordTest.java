a1364d8e73f69e767762ff6336500510e0196426
Erik-SmiT-V <eriksmitventurahernandez9@gmail.com>
2026-06-08 21:28:37 -0500

    modificacion a los test

--- src/test/java/com/backdea365/app/repository/RepositorioResetPasswordTest.java
+++ src/test/java/com/backdea365/app/repository/RepositorioResetPasswordTest.java
@@ -3,7 +3,7 @@ package com.backdea365.app.repository;
 import com.backdea365.app.model.ResetPassword;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase; // <-- IMPORTANTE
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
--- src/test/java/com/backdea365/app/service/ServicioAdminTest.java
+++ src/test/java/com/backdea365/app/service/ServicioAdminTest.java
@@ -1,6 +1,6 @@
 package com.backdea365.app.service;
 
-import com.backdena365.app.dto.AdminDTO;
+import com.backdea365.app.dto.AdminDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
